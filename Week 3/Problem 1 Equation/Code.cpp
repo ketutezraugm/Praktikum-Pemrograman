5c1a8efd904d69ac0af10a6a409cddf544d47da1
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-09-17 12:47:14 +0700

    Create Code.cpp

---
+++ Week 3/Problem 1 Equation/Code.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    // variables
+    int a,b,c; 
+    
+    // input
+    cout << "Enter value of a : "; 
+    cin >> a; 
+    cout << "\nEnter value of b : "; 
+    cin >> b;
+    cout << "\nEnter value of c : "; 
+    cin >> c; /
+
+    // operations
+    float disc = (b*b) - (4*a*c); 
+    
+    if (disc > 0) { 
+        float sqdisc = sqrt(disc);
+        float xa = (-b + sqdisc) / (2*a);
+        float xb = (-b - sqdisc) / (2*a); 
+        cout << "\nThe first root is " << xa; 
+        cout << "\nThe second root is " << xb; 
+    }
+    else if (disc == 0) { 
+        float x = -b/(2*a); 
+        cout << "\nThe root is " << x; 
+    }
+    else if (disc < 0) {
+        cout << "\nThere are no real roots (the roots are complex)";
+    }
+    
+    return 0;
+}
