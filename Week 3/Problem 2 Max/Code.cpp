a984e21fc95e5816a70bdd06735290db1a5010cb
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-09-17 12:48:10 +0700

    Create Code.cpp

---
+++ Week 3/Problem 2 Max/Code.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // variables
+    int num, high, i; 
+    
+    // input
+    for (i=0; i < 3; i++) {
+    cout << "Enter a number : "; 
+    cin >> num; 
+    
+    if (num > high) {
+        high = num; 
+    }
+    }
+    
+    cout << "The largest number is : " << high;
+    
+    return 0;
+}
