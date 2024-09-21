d57aa148917adaf44c39155120c5bbfa25d2cd23
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-09-22 02:09:55 +0700

    Update max.cpp

--- Week 3/Problem 2 Max/max.cpp
+++ Week 3/Problem 2 Max/max.cpp
@@ -9,13 +9,14 @@ int main() {
     for (i=0; i < 3; i++) {
         cout << "Enter a number : "; 
         cin >> num; 
-    }
     
+    // check
     if (num > high) {
         high = num; 
     }
     }
     
+    // output
     cout << "The largest number is : " << high;
     
     return 0;
