992aac6084071f7125d16f68a28be02dc797d8bd
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-09-22 20:22:57 +0700

    Create fizzbuzz_reversed.cpp

---
+++ Week 5/fizzbuzz_reversed.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // variables
+    int i,j;
+    int size,arr;
+    vector<int> vector;
+    
+    // fizzbuzz
+    // print 1-100
+    for (i=1; i<=100;) {
+        for (j=0; j<10; j++) {
+            // print fizzbuzz
+            if (i%3==0 and i%5==0) {
+                vector.push_back(i);
+                cout << "FizzBuzz ";
+            }
+            // print fizz
+            else if(i%3==0) 
+                cout << "Fizz ";
+            // print buzz
+            else if (i%5==0) 
+                cout << "Buzz ";
+            // other
+            else 
+                cout << i << " ";
+            i++;
+        }
+        cout << endl;
+    }
+    cout << endl;
+    
+    // reversed array
+    // size of the vector
+    size = vector.size();
+    
+    // reversing the vector
+    int reversed_vector[size];
+    for (i = 1; i <= size; i++) 
+        reversed_vector[i] = vector[size - i];
+
+    // output the reversed vector
+    cout << "Reversed Vector: ";
+    for (i = 1; i <= size; i++) {
+        cout << reversed_vector[i] << " ";
+    }
+
+    return 0;
+}
