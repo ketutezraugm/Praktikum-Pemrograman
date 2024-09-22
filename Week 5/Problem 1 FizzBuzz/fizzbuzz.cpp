76ce2fa52a3c41d25c5b8bbb5fbea02ce6afc858
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-09-22 19:49:31 +0700

    Create fizzbuzz.cpp

---
+++ Week 5/Problem 1 FizzBuzz/fizzbuzz.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // variables
+    vector<int> vector;
+    
+    // print 1-100
+    for (int i=1; i<=100; i++) {
+        // print fizzbuzz
+        if (i%3==0 and i%5==0) {
+            vector.push_back(i);
+            cout << "FizzBuzz" << endl;
+        }
+        // print fizz
+        else if(i%3==0)
+            cout << "Fizz" << endl;
+        // print buzz
+        else if (i%5==0)
+            cout << "Buzz" << endl;
+        // other
+        else 
+            cout << i << endl;
+    }
+    
+  
+
+    return 0;
+}
