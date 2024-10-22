86e8e2adc830bdaedcb1ca8513b0f78c44bb7dc9
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-10-22 21:39:35 +0700

    Create add_diff.cpp

---
+++ Week 7/Problem 1 Add Diff/add_diff.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void add(int a,int b) {
+    cout << a << " + " << b << " = " << a+b;
+}
+
+void diff(int a,int b) {
+    cout << a << " - " << b << " = " << a-b;
+}
+
+int main() {
+    // variables
+    int a,b,c;
+    string op;
+    
+    // inputs
+    cout << "Enter a number: ";
+    cin >> a;
+    cout << "Enter a number: ";
+    cin >> b;
+    cout << "Select the operation (+/-): ";
+    cin >> op;
+    
+    // process
+    if (op=="+")
+        add(a,b);
+    else if (op=="-")
+        diff(a,b);
+    
+    return 0;
+}
