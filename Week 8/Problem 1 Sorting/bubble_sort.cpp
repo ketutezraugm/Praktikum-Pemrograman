38e0800bdab5bff69aea6ca4cb27898d341045c6
ketutezraugm <iketutezrahesperosmaska@mail.ugm.ac.id>
2024-11-02 20:43:48 +0700

    Update bubble_sort.cpp

--- Week 8/Problem 1 Sorting/bubble_sort.cpp
+++ Week 8/Problem 1 Sorting/bubble_sort.cpp
@@ -10,22 +10,24 @@ struct student {
     int value;
 };
 
+// sorted based on NISN
 void bubble_sort_NISN(vector<student> &students) {
     int datasize = students.size();
-    for (int i=0; i<datasize-1; i++) {
-        for (int j=0; j<datasize-1; j++) {
-            if (students[j].NISN<students[j+1].NISN) 
-                swap(students[j], students[j+1]);
+    for (int i = 0; i < datasize - 1; i++) {
+        for (int j = 0; j < datasize - 1; j++) {
+            if (students[j].NISN < students[j + 1].NISN) 
+                swap(students[j], students[j + 1]);
         }
     }
 }
 
+// sorted based on value
 void bubble_sort_value(vector<student> &students) {
     int datasize = students.size();
-    for (int i=0; i<datasize-1; i++) {
-        for (int j=0; j<datasize-1; j++) {
-            if (students[j].value<students[j+1].value) 
-                swap(students[j], students[j+1]);
+    for (int i = 0; i < datasize - 1; i++) {
+        for (int j = 0; j < datasize - 1; j++) {
+            if (students[j].value < students[j + 1].value) 
+                swap(students[j], students[j + 1]);
         }
     }
 }
@@ -43,17 +45,17 @@ int main() {
     };
     int datasize = students.size();
 
-    // sorted based on NISN
+    // output sorted based on NISN
     bubble_sort_NISN(students);
     cout << "NISN Bubble Sort (Descending): " << endl;
-    for (int i=0; i<datasize; i++) {
+    for (int i = 0; i < datasize; i++) {
         cout << students[i].NISN << ", " << students[i].name << ", " << students[i].value << endl;
     }
 
-    // sorted based on value
+    // output sorted based on value
     bubble_sort_value(students);
     cout << "\nValue Bubble Sort (Descending): " << endl;
-    for (int i=0; i<datasize; i++) {
+    for (int i = 0; i < datasize; i++) {
         cout << students[i].NISN << ", " << students[i].name << ", " << students[i].value << endl;
     }
 
